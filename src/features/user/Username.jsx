8e74414e1d9daedd49a493b95627b84af1b568b9
Dal Shamsher Garbuja Pun <7892627+dal-gp@users.noreply.github.com>
2026-08-25 18:43:44 +1000

    style: add flexbox layout to Header and CartOverview, show Username at md+

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -2,7 +2,7 @@
  * Hardcoded username for now - will read from Redux store later
  */
 function Username() {
-  return <div className="hidden text-sm font-semibold">Dal</div>;
+  return <div className="hidden text-sm font-semibold md:block">Dal</div>;
 }
 
 export default Username;
